3c56962106cb8027f980747e4d3081da88d44b03
Hojat ghanbarzadeh <57577286+hojat2079@users.noreply.github.com>
2022-05-02 02:04:19 +0430

    fix landscape mode

--- lib/screen/translator.dart
+++ lib/screen/translator.dart
@@ -29,158 +29,161 @@ class _TranslatorPageState extends State<TranslatorPage> {
     return Scaffold(
       backgroundColor: Colors.blue,
       body: SafeArea(
-        child: Column(
-          children: [
-            const SizedBox(
-              height: 150,
-              child: Center(
-                child: Text(
-                  'اپلیکیشن مترجم',
-                  style: TextStyle(
-                    fontSize: 30,
-                    fontWeight: FontWeight.w500,
-                    fontFamily: 'CustomFont',
-                    color: Colors.white,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 150,
+                child: Center(
+                  child: Text(
+                    'اپلیکیشن مترجم',
+                    style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.w500,
+                      fontFamily: 'CustomFont',
+                      color: Colors.white,
+                    ),
                   ),
                 ),
               ),
-            ),
-            Expanded(
-              child: Container(
-                padding: const EdgeInsets.symmetric(horizontal: 32),
-                decoration: const BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(40),
-                        topRight: Radius.circular(40))),
-                child: Column(
-                  children: [
-                    const SizedBox(
-                      height: 50,
-                    ),
-                    Row(
-                      children: [
-                        Expanded(
-                            child: CustomInputField(
-                          title: 'From',
-                          hint: selectFromLanguage,
-                          widget: _customDropDown(
-                              list: languages,
-                              iconData: Icons.keyboard_arrow_down_outlined,
-                              fromLanguage: true),
-                        )),
-                        const SizedBox(
-                          width: 8,
-                        ),
-                        Expanded(
-                          child: CustomInputField(
-                            title: 'To',
-                            hint: selectToLanguage,
+              Expanded(
+                child: Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 32),
+                  decoration: const BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(40),
+                          topRight: Radius.circular(40))),
+                  child: Column(
+                    children: [
+                      const SizedBox(
+                        height: 50,
+                      ),
+                      Row(
+                        children: [
+                          Expanded(
+                              child: CustomInputField(
+                            title: 'From',
+                            hint: selectFromLanguage,
                             widget: _customDropDown(
-                                list: languages.reversed.toList(),
+                                list: languages,
                                 iconData: Icons.keyboard_arrow_down_outlined,
-                                fromLanguage: false),
+                                fromLanguage: true),
+                          )),
+                          const SizedBox(
+                            width: 8,
                           ),
-                        ),
-                      ],
-                    ),
-                    const SizedBox(
-                      height: 32,
-                    ),
-                    Directionality(
-                      textDirection: languageType == LanguageType.fa
-                          ? TextDirection.rtl
-                          : TextDirection.ltr,
-                      child: TextFormField(
-                        controller: txtTranslate,
-                        style: const TextStyle(color: Colors.black),
-                        decoration: InputDecoration(
-                          contentPadding: const EdgeInsets.symmetric(
-                              horizontal: 24, vertical: 16),
-                          isDense: true,
-                          label: Text(
-                            languageType == LanguageType.fa
-                                ? 'متن ورودی'
-                                : 'Enter the Word',
-                            style: TextStyle(
-                                fontSize: 18,
-                                fontFamily: languageType == LanguageType.fa
-                                    ? 'CustomFont'
-                                    : GoogleFonts.lato().fontFamily),
+                          Expanded(
+                            child: CustomInputField(
+                              title: 'To',
+                              hint: selectToLanguage,
+                              widget: _customDropDown(
+                                  list: languages.reversed.toList(),
+                                  iconData: Icons.keyboard_arrow_down_outlined,
+                                  fromLanguage: false),
+                            ),
                           ),
-                          enabledBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(50),
-                              borderSide: BorderSide(
-                                  width: 1,
-                                  color: Colors.blue.withOpacity(0.5))),
-                          focusedBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(50),
-                              borderSide: const BorderSide(
-                                  width: 1, color: Colors.blue)),
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 32,
+                      ),
+                      Directionality(
+                        textDirection: languageType == LanguageType.fa
+                            ? TextDirection.rtl
+                            : TextDirection.ltr,
+                        child: TextFormField(
+                          controller: txtTranslate,
+                          style: const TextStyle(color: Colors.black),
+                          decoration: InputDecoration(
+                            contentPadding: const EdgeInsets.symmetric(
+                                horizontal: 24, vertical: 16),
+                            isDense: true,
+                            label: Text(
+                              languageType == LanguageType.fa
+                                  ? 'متن ورودی'
+                                  : 'Enter the Word',
+                              style: TextStyle(
+                                  fontSize: 18,
+                                  fontFamily: languageType == LanguageType.fa
+                                      ? 'CustomFont'
+                                      : GoogleFonts.lato().fontFamily),
+                            ),
+                            enabledBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(50),
+                                borderSide: BorderSide(
+                                    width: 1,
+                                    color: Colors.blue.withOpacity(0.5))),
+                            focusedBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(50),
+                                borderSide: const BorderSide(
+                                    width: 1, color: Colors.blue)),
+                          ),
                         ),
                       ),
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    ElevatedButton(
-                        onPressed: () {
-                          if (selectFromLanguage == selectToLanguage) {
-                            setState(() {
-                              data = '';
-                            });
-                            showSnackBar(context,
-                                'زبان ورودی و خروجی نمیتواند یکسان باشد!!');
-                            return;
-                          }
-                          if (txtTranslate.text.isNotEmpty) {
-                            translator
-                                .translate(txtTranslate.text,
-                                    from: languageType == LanguageType.en
-                                        ? 'en'
-                                        : 'fa',
-                                    to: languageType == LanguageType.en
-                                        ? 'fa'
-                                        : 'en')
-                                .then((value) {
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      ElevatedButton(
+                          onPressed: () {
+                            if (selectFromLanguage == selectToLanguage) {
                               setState(() {
-                                data = value.text;
+                                data = '';
                               });
-                            });
-                          }
-                        },
-                        style: ButtonStyle(
-                            fixedSize: MaterialStateProperty.all(
-                                const Size.fromHeight(50)),
-                            shape: MaterialStateProperty.all(
-                                RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(50)))),
-                        child: const Center(
-                          child: Text(
-                            'ترجمه',
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontFamily: 'CustomFont',
-                                fontSize: 18,
-                                fontWeight: FontWeight.w700),
-                          ),
-                        )),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    Text(
-                      data,
-                      style: const TextStyle(
-                        fontSize: 18,
-                        fontFamily: 'CustomFont',
-                        fontWeight: FontWeight.w700,
+                              showSnackBar(context,
+                                  'زبان ورودی و خروجی نمیتواند یکسان باشد!!');
+                              return;
+                            }
+                            if (txtTranslate.text.isNotEmpty) {
+                              translator
+                                  .translate(txtTranslate.text,
+                                      from: languageType == LanguageType.en
+                                          ? 'en'
+                                          : 'fa',
+                                      to: languageType == LanguageType.en
+                                          ? 'fa'
+                                          : 'en')
+                                  .then((value) {
+                                setState(() {
+                                  data = value.text;
+                                });
+                              });
+                            }
+                          },
+                          style: ButtonStyle(
+                              fixedSize: MaterialStateProperty.all(
+                                  const Size.fromHeight(50)),
+                              shape: MaterialStateProperty.all(
+                                  RoundedRectangleBorder(
+                                      borderRadius:
+                                          BorderRadius.circular(50)))),
+                          child: const Center(
+                            child: Text(
+                              'ترجمه',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontFamily: 'CustomFont',
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.w700),
+                            ),
+                          )),
+                      const SizedBox(
+                        height: 20,
                       ),
-                    )
-                  ],
+                      Text(
+                        data,
+                        style: const TextStyle(
+                          fontSize: 18,
+                          fontFamily: 'CustomFont',
+                          fontWeight: FontWeight.w700,
+                        ),
+                      )
+                    ],
+                  ),
                 ),
-              ),
-            )
-          ],
+              )
+            ],
+          ),
         ),
       ),
     );
